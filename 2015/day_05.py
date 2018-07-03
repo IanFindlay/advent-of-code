edf45e088992034bffe4a797c2b8d5b8bdf0e363
IFinners <IDFinners@Gmail.com>
2018-07-03 20:36:40 +0100

    Add day_05.py

---
+++ 2015/day_05.py
@@ -0,0 +1,29 @@
+"""Advent of Code Day 5 - Doesn't He Have Intern-Elves For This?"""
+
+import re
+
+with open('input.txt') as f:
+    strings = f.readlines()
+
+# Answer One Regexes
+naughty_regex = re.compile(r'ab|cd|pq|xy')
+vowel_regex = re.compile(r'([aeiou].*){3,}')
+double_regex = re.compile(r'(.)\1')
+# Answer Two Regexes
+repeated_regex = re.compile(r'(..).*\1')
+gapped_regex = re.compile(r'(.).\1')
+
+nice = 0
+nice_2 = 0
+for string in strings:
+
+    if (vowel_regex.search(string) and double_regex.search(string)
+            and not naughty_regex.search(string)):
+        nice += 1
+
+    if repeated_regex.search(string) and gapped_regex.search(string):
+        nice_2 += 1
+
+
+print("Answer One =", nice)
+print("Answer Two =", nice_2)
