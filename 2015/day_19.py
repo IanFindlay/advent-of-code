20075b5085060b4651724fc1901d98fb8ecb05eb
IFinners <IDFinners@Gmail.com>
2018-07-12 16:05:06 +0100

    Add part two answer to day 19

--- 2015/day_19.py
+++ 2015/day_19.py
@@ -14,18 +14,42 @@ for replacement in replacements:
     info = replacement.split(' ')
     change_from = info[0]
     change_to = info[2]
-    
+
     i = 0
     while i < len(mol):
         if mol[i: i + len(change_from)] == change_from:
             new_mol = mol[:i] + change_to + mol[i + len(change_from):]
             i += len(change_from)
-            new_list.add(new_mol)      
+            new_list.add(new_mol)
 
         else:
             i += 1
 
-# Answer Part One  
+# Answer Part One
 print("Number of Distinct Molecules After One Replacement = " + str(len(new_list)))
 
 # Part Two
+
+# Function so it can be fed to re.sub
+def replace(rev_chem):
+    """Return the appropriate substitution of an element string."""
+    return unsynth[rev_chem.group()]
+
+
+# Reverse to capture Ar subs before other part of the sub are carried out
+rev_mol = mol[::-1]
+
+rep_regex = re.compile(r'(\w+) => (\w+)')
+reps = rep_regex.findall('\n'.join(replacements))
+
+# Build dict of reversed elements - k = replacement and v = replaced
+unsynth = {}
+for rep in reps:
+    unsynth[rep[1][::-1]] = rep[0][::-1]
+
+count = 0
+while rev_mol != 'e':
+    rev_mol = re.sub('|'.join(unsynth.keys()), replace, rev_mol, 1)
+    count += 1
+
+print("Number of Steps to Synthesise Rudolph Cure =", count)
