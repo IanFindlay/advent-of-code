d5bad59f3c5e76cd852a99d322a4becdb403abfb
IFinners <IDFinners@Gmail.com>
2018-07-17 17:37:05 +0100

    Add day_20.py

---
+++ 2016/day_20.py
@@ -0,0 +1,34 @@
+"""Advent of Code Day 20 - Firewall Rules"""
+
+with open('input.txt') as f:
+    ip_ranges = [line.strip() for line in f.readlines()]
+
+blacklisted = []
+for ip_range in ip_ranges:
+    parse = ip_range.split('-')
+    start, end = int(parse[0]), int(parse[1])
+    blacklisted.append((start, end))
+
+lowest = None
+allowed = 0
+target = 0
+while target <= 4294967295:
+    unblocked = True
+    for listed in blacklisted:
+        if target >= listed[0] and target <= listed[1]:
+            target = listed[1]
+            unblocked = False
+            break
+
+    if unblocked:
+        allowed += 1
+        if not lowest:
+            lowest = target
+
+    target += 1
+
+# Answer One
+print("Lowest unblocked IP:", lowest)
+
+# Answer Two
+print("Unblocked IPs:", allowed)
