669abf683bc79005c14b7a1aed5af95483459211
IFinners <IDFinners@Gmail.com>
2018-07-23 16:40:42 +0100

    Rewrite and complete day_07.py

--- 2017/day_07.py
+++ 2017/day_07.py
@@ -1,39 +1,60 @@
 """Advent of Code Day 7 - Recursive Circus"""
 
+import collections
 import re
-import pyperclip
 
 
-def tower_info(structure, balance_fix=False):
-    """Find base program of structure and how to fix the tower imbalance."""
-    # Isolate the program names
-    names_regex = re.compile(r'[^0-9\s()->]\w*')
-    programs = names_regex.findall(structure)
+def base_dict(tower_data):
+    """Build weight and holding dictionaries and identify the base program."""
+    holding = {}
+    weights = {}
+    base = []
+    for tower in tower_data:
+        program, weight, *held = re.findall(r'\w+', tower)
+        base.append(program)
+        weights[program] = int(weight)
+        holding[program] = list(held)
 
-    # Find the base (has no parents so program is referenced only once)
-    for program in programs:
-        base_regex = re.compile(r'{0}'.format(program))
-        mentions = base_regex.findall(structure)
-        if len(mentions) == 1:
-            base = program
+    for __, values in holding.items():
+        [base.remove(value) for value in values]
 
-    # Return Base Program
-    if not balance_fix:
-        return base
-
-    # Split structure into program name: weight dictionary
-    dict_regex = re.compile(r'([^0-9\s()->]\w*)\s\((\d*)')
-    weights = dict_regex.findall(structure)
-    prog_weight = {}
-    for program in weights:
-        name = program[0]
-        weight = program[1]
-        prog_weight[name] = weight
-    print(prog_weight)
+    return (holding, weights, base[0])
 
 
-# Answer One
-print("Base Program:", tower_info(pyperclip.paste()))
+def find_fix(program):
+    """Traverse structure and find mismatched weight and the fix required."""
+    above = [find_fix(held) for held in holding[program]]
 
-# Answer Two
-print(tower_info(pyperclip.paste(), balance_fix='yes'))
+    if len(set(above)) == 2:
+        # Imbalanced so find most common (correct) weight and aberrant one
+        mismatch = collections.Counter(above).most_common()
+        desired = mismatch[0][0]
+        wrong = mismatch[1][0]
+        # Find imbalanced program and the weight it should be
+        imbalanced = holding[program][above.index(wrong)]
+        corrected = weights[imbalanced] + (desired - wrong)
+
+        # Answer Two
+        print("Corrected weight:", corrected)
+
+        # Return corrected weight so upstream imbalances don't trigger
+        return weights[program] + sum(above) + desired - wrong
+
+    return weights[program] + sum(above)
+
+
+if __name__ == '__main__':
+
+    with open('input.txt', 'r') as f:
+        data = f.readlines()
+
+        initial_info = base_dict(data)
+        holding = initial_info[0]
+        weights = initial_info[1]
+        base = initial_info[2]
+
+        # Answer One
+        print("Bottom program:", base)
+
+        # Answer Two
+        find_fix(base)
