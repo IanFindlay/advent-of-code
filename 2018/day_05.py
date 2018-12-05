07c1d7bd75d2b11e434b63f39d58119e170c0349
IFinners <IDFinners@Gmail.com>
2018-12-05 18:35:14 +0000

    Add day_05.py

---
+++ 2018/day_05.py
@@ -0,0 +1,38 @@
+"""Advent of Code Day 5 - Alchemical Reduction"""
+
+
+def react_polymer(polymer):
+    """Return polymer after all unit reactions have taken place."""
+    prev_poly = None
+    while prev_poly != polymer:
+        prev_poly = polymer
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            upper = letter.upper()
+            polymer = polymer.replace('{}{}'.format(letter, upper), '')
+            polymer = polymer.replace('{}{}'.format(upper, letter), '')
+
+    return polymer
+
+
+if __name__ == '__main__':
+
+    with open('input.txt') as f:
+        polymer = f.read()
+
+    reduced_polymer = react_polymer(polymer)
+    reacted_length = len(reduced_polymer)
+
+    # Answer One
+    print("Length after reduction:", reacted_length)
+
+    shortest = reacted_length
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        new_polymer = reduced_polymer.replace(letter, '')
+        new_polymer = new_polymer.replace(letter.upper(), '')
+
+        reacted_length = len(react_polymer(new_polymer))
+        if reacted_length < shortest:
+            shortest = reacted_length
+
+    # Answer Two
+    print("Shortest polymer:", shortest)
