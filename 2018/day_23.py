1061d8b526e093ccf8ff7866102857f7d14b1f94
IFinners <IDFinners@Gmail.com>
2019-01-23 16:37:34 +0000

    Refactor some comments into docstrings and other tidying up

--- 2018/day_23.py
+++ 2018/day_23.py
@@ -42,29 +42,28 @@ def split_box(low, high):
 
 
 def bots_in_range(low, high, nanobots):
-    """Return how many nanobots are in range of the box."""
+    """Return how many nanobots are in range of the box.
+
+       Any point inside the box will have the property that the summed
+       distance from that point to both the low and high opposite corners
+       will be equal to the distance between the corners.
+       In the case of points outside of the box, the above allows the distance
+       from that point to the nearest point on the box to be found as that
+       distance is the distance from the point to each of the corners minus the
+       corner to corner distance (as that is the distance from the nearest
+       point to the corners and so can be discarded). This distance is twice
+       the actual value as the calculation used both corners but the nearest
+       point only needs to be reached once.
+       If this distance is <= the nanobots radius, the nanobot is in range.
+    """
     # Low of one box can be high of another - adjusted_high avoids this overlap
-    adjusted_high = (high[0] - 1, high[1] - 1, high[2] - 1)
+    adjusted_high = tuple([coord - 1 for coord in high])
     in_range = 0
     for bot in nanobots.items():
-        # If the bot is in box the distances between the bots coords and each
-        # of the corners will sum to the distance between the corners
         coords, radius = bot
         dist_to_low = distance_between(coords, low)
         dist_to_high = distance_between(coords, adjusted_high)
         dist_low_high = distance_between(low, adjusted_high)
-
-        if dist_low_high == dist_to_low + dist_to_high:
-            in_range += 1
-            continue
-
-        # Check if radius brings bot into range - idea behind formula:
-        # Nearest point on box from bot position satisfies, by definition of
-        # being within the box, the condition used above.
-        # Therefore, the distance to the nearest point can be found using
-        # the bots distance to the corners minus the corner to corner distance
-        # (as you only need the closest points distance).
-        # This distance will be twice the answer due to using both corners.
         distance = (dist_to_low + dist_to_high - dist_low_high) // 2
         if radius >= distance:
             in_range += 1
@@ -73,9 +72,14 @@ def bots_in_range(low, high, nanobots):
 
 
 def main():
-    """Parse nanobots and explore the depth of coverage of the area."""
-    nanobots = parse_bots()
+    """Parse nanobots and explore the depth of coverage of the area.
 
+       Find the Nanobot with the largest radius and determine how many
+       nanobots are within that range. Then find the coordinate that is in the
+       range of the most nanobots, with a tie going to the one closest to the
+       origin, using an octree scan.
+    """
+    nanobots = parse_bots()
     max_radius = 0
     coords_of_max = None
     for coords, radius in nanobots.items():
@@ -84,7 +88,6 @@ def main():
             coords_of_max = coords
 
     in_range_of_max = 0
-    # Find how many nanobots are in range of bot with largest radius
     for nanobot in nanobots:
         distance = distance_between(coords_of_max, nanobot)
         if distance <= max_radius:
@@ -98,8 +101,9 @@ def main():
     furthest_coord = 0
     for coords, radius in nanobots.items():
         high_abs = max([abs(coord) for coord in coords])
-        if high_abs + radius > furthest_coord:
-            furthest_coord = high_abs + radius
+        coord_distance = high_abs + radius
+        if coord_distance > furthest_coord:
+            furthest_coord = coord_distance
 
     half_side_length = 1
     while half_side_length <= furthest_coord:
@@ -117,13 +121,13 @@ def main():
     boxes = [(in_range, side_length, initial_distance, low, high)]
 
     while True:
-        # Sort boxes by nanobots in range, size, distance from origin order
+        # Sort boxes by nanobots in range, size, distance from origin
         boxes.sort(key=lambda box: (box[0], box[1], box[2]))
         __, size, distance_from_origin, low, high = boxes.pop()
 
         # If box size == 1 low corner is the answer as sorting ensures that
         # there is no larger box with the same or more bots in range and
-        # any 1x1x1 bots with the same number of bots in range must be
+        # any 1x1x1 box with the same number of bots in range must be
         # further away from the origin
         if size == 1:
             distance_from_origin *= -1
