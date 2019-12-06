b12faa4a6f850f72ad290722931e085e1d612f0f
IFinners <IFinners@ProtonMail.com>
2019-12-06 12:12:58 +0000

    Add day_06.py and update README

---
+++ 2019/day_06.py
@@ -0,0 +1,39 @@
+"""Advent of Code 2019 Day 6 - Universal Orbit Map."""
+
+
+with open('input.txt', 'r') as f:
+    orbits = f.read().split()
+
+objects_dict = {}
+for orbit in orbits:
+    orbited, orbiter = orbit.split(')')
+    objects_dict[orbiter] = orbited
+
+num_orbits = 0
+for orbiter in objects_dict:
+    next_orbit = objects_dict.get(orbiter, None)
+    while next_orbit:
+        next_orbit = objects_dict.get(next_orbit, None)
+        num_orbits += 1
+
+# Answer One
+print("Number of direct and indirect orbits:", num_orbits)
+
+you_path = {}
+on_you_path = set()
+transfers = 0
+next_orbit = objects_dict.get("YOU", None)
+while next_orbit:
+    transfers += 1
+    you_path[next_orbit] = transfers
+    on_you_path.add(next_orbit)
+    next_orbit = objects_dict.get(next_orbit, None)
+
+transfers = 0
+next_orbit = objects_dict.get("SAN", None)
+while next_orbit and next_orbit not in on_you_path:
+    transfers += 1
+    next_orbit = objects_dict.get(next_orbit, None)
+
+# Answer Two
+print("Transfers between you and Santa:", transfers + you_path[next_orbit] - 1)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## My solutions to the [Advent of Code](https://adventofcode.com/) challenges by [Eric Wastl](https://github.com/topaz)
 
-### Overall Stars Collected (210/210):  
+### Overall Stars Collected (212/212):  
   
 * #### [2015](https://adventofcode.com/2015) - Stars Collected (50/50)  
 
@@ -12,4 +12,4 @@
 
 * #### [2018](https://adventofcode.com/2018) - Stars Collected (50/50)
 
-* #### [2019](https://adventofcode.com/2019) - Stars Collected (10/10)
+* #### [2019](https://adventofcode.com/2019) - Stars Collected (12/12)
