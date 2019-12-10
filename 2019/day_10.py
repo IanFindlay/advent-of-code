f5ff56f1ef89e1117cf03ea83c748b6e7984d3b0
IFinners <IFinners@ProtonMail.com>
2019-12-10 13:41:15 +0000

    Add finished day_10.py and update README

--- 2019/day_10.py
+++ 2019/day_10.py
@@ -1,6 +1,7 @@
 """Advent of Code 2019 Day 10 - Monitoring Station."""
 
-import math
+
+from math import atan2, degrees, pi
 
 
 with open("input.txt", "r") as f:
@@ -19,25 +20,57 @@ for row in space:
 
 detection_dict = {}
 for coords, value in space_dict.items():
-    los = []
+    los = {}
     for location, item in space_dict.items():
         if location == coords:
             continue
-        x , y = location
-        dx = x - coords[0]
-        dy = y - coords[1]
-        angle = math.atan2(dx,dy)
-
-        los.append(angle)
+        x, y = location
+        dx = coords[0] - x
+        dy = coords[1] - y
+        angle = atan2(dy, dx)
+        if angle < 0:
+            angle += 2 * pi
+        los[location] = degrees(angle)
 
     detection_dict[coords] = los
 
 most_detected = None
-for asteroid, inline in detection_dict.items():
-    num_detected = len(set(inline))
+for asteroid, los_dict in detection_dict.items():
+    num_detected = len(set(los_dict.values()))
     if not most_detected or num_detected > most_detected[1]:
         most_detected = (asteroid, num_detected)
 
 # Answer One
-print("Other asteroids detected from the best location:", most_detected)
+print("Other asteroids detected from the best location:", most_detected[1])
 
+station_coords = most_detected[0]
+station = detection_dict[station_coords]
+for asteroid, angle in station.items():
+    ast_x, ast_y = asteroid
+    distance = (
+        abs(station_coords[0] - ast_x) + abs(station_coords[1] - ast_y)
+    )
+    angle = (angle + 270) % 360   # Rotate so angles start at 12 O'Clock
+    station[asteroid] = (angle, distance)
+
+# Sort asteroids by angle, then distance
+sorted_asteroids = dict(
+    sorted(station.items(), key=lambda item: (item[1][0], item[1][1]))
+)
+
+vaporised_coords = set()
+last_angle = None
+vaporised = 0
+while vaporised != 200:
+    for asteroid, info in sorted_asteroids.items():
+        angle = info[0]
+        if angle == last_angle or asteroid in vaporised_coords:
+            continue
+        last_angle = angle
+        vaporised_coords.add(asteroid)
+        vaporised += 1
+        if vaporised == 200:
+            break
+
+# Answer Two
+print("Coordinates for 200th vaporised asteroid:", asteroid)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## My solutions to the [Advent of Code](https://adventofcode.com/) challenges by [Eric Wastl](https://github.com/topaz)
 
-### Overall Stars Collected (218/218):  
+### Overall Stars Collected (220/220):  
   
 * #### [2015](https://adventofcode.com/2015) - Stars Collected (50/50)  
 
@@ -12,4 +12,4 @@
 
 * #### [2018](https://adventofcode.com/2018) - Stars Collected (50/50)
 
-* #### [2019](https://adventofcode.com/2019) - Stars Collected (18/18)
+* #### [2019](https://adventofcode.com/2019) - Stars Collected (20/20)
