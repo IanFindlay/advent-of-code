0baa5c64e58df8ad686798507a1082ce743d88d8
IFinners <IFinners@ProtonMail.com>
2020-01-02 15:53:44 +0000

    Add day_20.py part one and update README

---
+++ 2019/day_20.py
@@ -0,0 +1,115 @@
+"""Advent of Code 2019 Day 20 - Donut Maze."""
+
+
+from collections import defaultdict, deque
+
+
+def maze_bfs(maze, start, end, portals):
+    """BFS from entrance to exit of maze with portals.
+
+        Args:
+            maze (dict): Coords: Value dictionary representing the maze.
+            entrance (str): Coords of entrance (x, y).
+            exit (str): Coords of exit (x, y).
+            portals (dict): Dictionary mapping portals to their destinations.
+
+        Returns:
+            Length of the shortest path (int)
+
+    """
+    visited = set()
+    visited.add(start)
+    queue = deque()
+    queue.append((start, 0))
+    while queue:
+        coords, steps = queue.popleft()
+
+        if coords in portals:
+            warp_to = portals[coords]
+            if warp_to not in visited:
+                visited.add(warp_to)
+                queue.append((warp_to, steps + 1))
+
+        x, y = coords
+        next_nodes = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        for node in next_nodes:
+            if node in visited:
+                continue
+
+            if node == end:
+                return steps + 1
+
+            tile_value = maze.get(node)
+
+            if tile_value == '.':
+                visited.add(node)
+                queue.append((node, steps + 1))
+
+    return False
+
+
+with open('input.txt') as f:
+    input_map = [line.strip('\n') for line in f.readlines()]
+
+maze_dict = {}
+for y in range(len(input_map)):
+    for x in range(len(input_map[0])):
+        maze_dict[(x, y)] = input_map[y][x]
+
+portal_locations = defaultdict(list)
+for coords, tile in maze_dict.items():
+    if tile in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        portal_code = tile
+        x, y = coords
+        neighbours = {
+            'l': (x - 1, y), 'r': (x + 1, y),
+            'd': (x, y + 1), 'u': (x, y - 1)
+        }
+        for direction, new_coords in neighbours.items():
+            portal_coords = None
+            value = maze_dict.get(new_coords, '#')
+            if value in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                if direction == 'l':
+                    portal_code = value + portal_code
+                elif direction == 'r':
+                    portal_code += value
+                elif direction == 'u':
+                    portal_code = value + portal_code
+                elif direction == 'd':
+                    portal_code += value
+
+            elif value == '.':
+                portal_coords = new_coords
+
+            else:
+                continue
+
+            if not portal_coords:
+                new_x, new_y = new_coords
+                adjacents = [
+                    (new_x - 1, new_y), (new_x + 1, new_y),
+                    (new_x, new_y - 1), (new_x, new_y + 1)
+                ]
+                for adjacent in adjacents:
+                    value = maze_dict.get(adjacent)
+                    if value == '.':
+                        portal_coords = adjacent
+                        break
+
+                if portal_coords not in portal_locations[portal_code]:
+                    if portal_coords:
+                        portal_locations[portal_code].append(portal_coords)
+
+portal_links = {}
+for code, coords in portal_locations.items():
+    if code == 'AA':
+        entrance = coords[0]
+    elif code == 'ZZ':
+        maze_exit = coords[0]
+    else:
+        portal_links[coords[0]] = coords[1]
+        portal_links[coords[1]] = coords[0]
+
+# Answer One
+fewest_steps = maze_bfs(maze_dict, entrance, maze_exit, portal_links)
+print("Fewest steps required to exit the maze:", fewest_steps)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## My solutions to the [Advent of Code](https://adventofcode.com/) challenges by [Eric Wastl](https://github.com/topaz)
 
-### Overall Stars Collected (236/238):  
+### Overall Stars Collected (237/250):  
   
 * #### [2015](https://adventofcode.com/2015) - Stars Collected (50/50)  
 
@@ -12,4 +12,4 @@
 
 * #### [2018](https://adventofcode.com/2018) - Stars Collected (50/50)
 
-* #### [2019](https://adventofcode.com/2019) - Stars Collected (36/38)
+* #### [2019](https://adventofcode.com/2019) - Stars Collected (37/50)
