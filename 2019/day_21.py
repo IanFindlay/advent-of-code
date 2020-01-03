eb338c5fcec8c391c396d4d71284b5886d109034
IFinners <IFinners@ProtonMail.com>
2020-01-03 12:22:02 +0000

    Add day_21.py part one

---
+++ 2019/day_21.py
@@ -0,0 +1,146 @@
+""" Advent of Code 2019 Day 21 - Springdroid Adventure."""
+
+
+from collections import defaultdict
+
+
+class SpringDroid:
+    """Class for the ASCII - intcode controlled Springdroid."""
+
+    def __init__(self, code):
+        """Initialises ASCII system code (dict) and an input list."""
+        self.code = code
+        self.inputs = []
+        self.pointer = 0
+
+    def run_intcode(self):
+        """Run intcode program on code."""
+        relative_base = 0
+        while True:
+            opcode = self.code[self.pointer]
+
+            if 99 < opcode <= 22210:
+                opcode, modes = parse_instruction(opcode)
+            else:
+                modes = [0, 0, 0]
+
+            if opcode == 99:   # halt
+                return
+
+            if modes[0] == 0:
+                param_1 = self.code[self.pointer + 1]
+            elif modes[0] == 1:
+                param_1 = self.pointer + 1
+            else:
+                param_1 = self.code[self.pointer + 1] + relative_base
+
+            if modes[1] == 0:
+                param_2 = self.code[self.pointer + 2]
+            elif modes[1] == 1:
+                param_2 = self.pointer + 2
+            else:
+                param_2 = self.code[self.pointer + 2] + relative_base
+
+            if modes[2] == 0:
+                param_3 = self.code[self.pointer + 3]
+            else:
+                param_3 = self.code[self.pointer + 3] + relative_base
+
+            if opcode == 1:   # addition
+                self.code[param_3] = self.code[param_1] + self.code[param_2]
+                self.pointer += 4
+
+            elif opcode == 2:   # multiplication
+                self.code[param_3] = self.code[param_1] * self.code[param_2]
+                self.pointer += 4
+
+            elif opcode == 3:   # input
+                if self.inputs:
+                    self.code[param_1] = self.inputs.pop(0)
+                    self.pointer += 2
+                else:
+                    yield "Waiting on input"
+
+            elif opcode == 4:   # output
+                self.pointer += 2
+                output = self.code[param_1]
+                yield output
+
+            elif opcode == 5:   # jump-if-true
+                if self.code[param_1] == 0:
+                    self.pointer += 3
+                else:
+                    self.pointer = self.code[param_2]
+
+            elif opcode == 6:   # jump-if-false
+                if self.code[param_1] != 0:
+                    self.pointer += 3
+                else:
+                    self.pointer = self.code[param_2]
+
+            elif opcode == 7:   # less than
+                if self.code[param_1] < self.code[param_2]:
+                    self.code[param_3] = 1
+                else:
+                    self.code[param_3] = 0
+                self.pointer += 4
+
+            elif opcode == 8:   # equals
+                if self.code[param_1] == self.code[param_2]:
+                    self.code[param_3] = 1
+                else:
+                    self.code[param_3] = 0
+                self.pointer += 4
+
+            elif opcode == 9:   # adjust relative base
+                relative_base += self.code[param_1]
+                self.pointer += 2
+
+            else:
+                print("Invalid or incorrectly parsed instruction.")
+                return
+
+
+def parse_instruction(value):
+    """Return opcode and mode parsed from instruction value."""
+    str_value = str(value)
+    opcode = int(str_value[-2:])
+    modes = [int(x) for x in list(str_value)[:-2]]
+    while len(modes) != 3:
+        modes.insert(0, 0)
+    return (opcode, list(reversed(modes)))
+
+
+intcode_dict = defaultdict(int)
+with open('input.txt') as f:
+    i = 0
+    for instruction in f.read().split(','):
+        intcode_dict[i] = int(instruction)
+        i += 1
+
+bot = SpringDroid(intcode_dict.copy())
+assess_hull = bot.run_intcode()
+
+# NOT C T - If 3rd space is a hole prepare to jump with temp
+# AND D T - Unless 4th space is a hole
+# NOT A J - If 1st tile is a hole then jump
+# OR T J - If temp or jump true then jump
+
+instructions = """NOT C T
+    AND D T
+    NOT A J
+    OR T J
+    WALK
+
+"""
+
+bot.inputs = list(map(ord, instructions))
+while True:
+    try:
+        output = next(assess_hull)
+        print(chr(output), end='')
+    except ValueError:
+        break
+
+# Answer One
+print("Reported hull damage:", output)
