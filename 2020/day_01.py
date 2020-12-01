666566ce597bb27dc3cdba95c0e19b6b575d36cf
Ian Findlay <code@findlayian.com>
2020-12-01 15:46:37 +0000

    Cleanup code slightly

--- 2020/day_01.py
+++ 2020/day_01.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 """Advent of Code 2020 Day 01 - Report Repair."""
 
 
-with open ('input.txt', 'r') as f:
-    entries = set([int(entry) for entry in f.readlines()])
+with open ('input.txt', 'r') as expense_report:
+    entries = set([int(line) for line in expense_report.readlines()])
 
 for entry in entries:
     if 2020 - entry in entries:
@@ -10,20 +12,18 @@ for entry in entries:
         break
 
 # Answer One
-print("Two entries that add to 2020", answer_one)
+print("Product of the two entries that add to 2020:", answer_one)
 
+answer_two = False
 for entry in entries:
-    target = 2020 - entry
-    for entry_2 in entries:
-        answer_two = False
-        if entry_2 == entry:
-            pass
-        if target - entry_2 in entries:
-            answer_two = entry * entry_2 * (target - entry_2)
+    remaining_amount = 2020 - entry
+    for entry_two in entries:
+        if remaining_amount - entry_two in entries and entry_two != entry:
+            answer_two = entry * entry_two * (remaining_amount - entry_two)
             break
 
     if answer_two:
         break
 
 # Answer Two
-print("Three entries that add to 2020", answer_two)
+print("Product of the three entries that add to 2020:", answer_two)
