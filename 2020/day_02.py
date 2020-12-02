a3b208728af89e4b7b3e65a2d1d82ab038916175
Ian Findlay <code@findlayian.com>
2020-12-02 14:35:32 +0000

    Refactor into combined solution that uses regex

--- 2020/day_02.py
+++ 2020/day_02.py
@@ -2,36 +2,33 @@
 
 """Advent of Code 2020 Day 02 - Password Philosophy."""
 
+import re
+
 
 with open ('input.txt', 'r') as password_database:
     lines = [line for line in password_database.readlines()]
 
-valid_passwords = 0
+line_regex = re.compile('(\d*)-(\d*)\s(\w):\s(\w*)')
+part_one_valid = 0
+part_two_valid = 0
 for line in lines:
-    policy, password = line.split(':')
-    range_must_appear, character = policy.split(' ')
-    lowest_repeat, highest_repeat = range_must_appear.split('-')
-    char_count = password.count(character)
-    if char_count >= int(lowest_repeat) and char_count <= int(highest_repeat):
-        valid_passwords += 1
+    first_num, second_num, char, password = line_regex.findall(line)[0]
+    first_num = int(first_num)
+    second_num = int(second_num)
+
+    # Part One
+    char_count = password.count(char)
+    if char_count >= first_num and char_count <= second_num:
+        part_one_valid += 1
+
+    # Part Two
+    if password[first_num - 1] == char and password[second_num - 1] != char:
+        part_two_valid += 1
+    elif password[first_num - 1] != char and password[second_num -1] == char:
+        part_two_valid += 1
 
 # Answer One
-print("Number of valid passwords:", valid_passwords)
-
-
-valid_passwords = 0
-for line in lines:
-    policy, password = line.split(':')
-    password = password.lstrip()
-    indicies, char = policy.split(' ')
-    lowest_index, highest_index = indicies.split('-')
-    lowest_index = int(lowest_index) - 1
-    highest_index = int(highest_index) - 1
-
-    if password[lowest_index] == char and password[highest_index] != char:
-        valid_passwords += 1
-    elif password[lowest_index] != char and password[highest_index] == char:
-        valid_passwords += 1
+print("Number of valid passwords according to first policy:", part_one_valid)
 
 # Answer Two
-print("Number of valid passwords:", valid_passwords)
+print("Number of valid passwords according to second policy:", part_two_valid)
