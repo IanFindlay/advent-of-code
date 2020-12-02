4195ef2c81685324948a230c949af55778b16951
Ian Findlay <code@findlayian.com>
2020-12-02 11:19:40 +0000

    Add solution to part two

--- 2020/day_02.py
+++ 2020/day_02.py
@@ -17,3 +17,21 @@ for line in lines:
 
 # Answer One
 print("Number of valid passwords:", valid_passwords)
+
+
+valid_passwords = 0
+for line in lines:
+    policy, password = line.split(':')
+    password = password.lstrip()
+    indicies, char = policy.split(' ')
+    lowest_index, highest_index = indicies.split('-')
+    lowest_index = int(lowest_index) - 1
+    highest_index = int(highest_index) - 1
+
+    if password[lowest_index] == char and password[highest_index] != char:
+        valid_passwords += 1
+    elif password[lowest_index] != char and password[highest_index] == char:
+        valid_passwords += 1
+
+# Answer Two
+print("Number of valid passwords:", valid_passwords)
