23201b5e13f6df03fdf67ecb0e4d3a887e29fab5
Ian Findlay <code@findlayian.com>
2020-12-05 17:18:23 +0000

    Refactor - functions combined, updated logic

--- 2020/day_05.py
+++ 2020/day_05.py
@@ -3,70 +3,61 @@
 """Advent of Code 2020 Day 05 - Binary Binding."""
 
 
-def decode_row(code: str) -> int:
-    """."""
-    range_low = 0
-    range_high = 127
+def decode_binary_space_partition(code: str) -> tuple:
+    """Decode binary partition character code and return (row, col) tuple.
+
+    Args:
+        code: A 9 character code - The first 7 characters are each either
+              "F"orward or "B"ack and the last 3 are either "L"eft or "R"ight.
+
+    Returns:
+        tuple consisting of two integers representing row and column.
+
+    """
+    row_low = col_low = 0
+    row_high = 127
+    col_high = 7
     for char in code:
         if char == "F":
-            range_high -= (range_high - range_low) // 2 + 1
+            row_high -= (row_high - row_low) // 2 + 1
         elif char == "B":
-            range_low += (range_high - range_low) // 2 + 1
-
-    return range_low if char == "F" else range_high
-
-
-def decode_col(code: str) -> int:
-    """."""
-    range_low = 0
-    range_high = 7
-    for char in code:
-        if char == "L":
-            range_high -= (range_high - range_low) // 2 + 1
+            row_low += (row_high - row_low) // 2 + 1
+        elif char == "L":
+            col_high -= (col_high - col_low) // 2 + 1
         elif char == "R":
-            range_low += (range_high - range_low) // 2 + 1
+            col_low += (col_high - col_low) // 2 + 1
 
-    return range_low if char == "L" else range_high
+    row = row_low if code[6] == "F" else row_high
+
+    col = col_low if code[9] == "L" else col_high
+
+    return (row, col)
 
 
 with open ('input.txt', 'r') as scanned:
     seat_codes = [seat_code.strip() for seat_code in scanned.readlines()]
 
-highest_id = 0
-for seat_code in seat_codes:
-    row_code = seat_code[:7]
-    col_code = seat_code[7:]
-    seat_row = decode_row(row_code)
-    seat_col = decode_col(col_code)
-    seat_id = seat_row * 8 + seat_col
-
-    if seat_id > highest_id:
-        highest_id = seat_id
-
-# Answer One
-print("Highest seat ID scanned:", highest_id)
-
-all_seats = set()
-for row in range(1, 127):
-    for col in range(0, 8):
-        all_seats.add((row, col))
-
-possible_seat = set(all_seats)
+seats = set()
 seat_ids = set()
 for seat_code in seat_codes:
-    row_code = seat_code[:7]
-    col_code = seat_code[7:]
-    seat_row = decode_row(row_code)
-    seat_col = decode_col(col_code)
-    seat_id = seat_row * 8 + seat_col
+    row, col = decode_binary_space_partition(seat_code)
+    seats.add((row, col))
+    seat_id = row * 8 + col
     seat_ids.add(seat_id)
 
-    possible_seat.remove((seat_row, seat_col))
+# Answer One
+print("Highest seat ID scanned:", max(seat_ids))
 
-for seat in possible_seat:
-    seat_id = seat[0] * 8 + seat[1]
-    if seat_id - 1 in seat_ids and seat_id + 1 in seat_ids:
-        my_seat_id = seat_id
+my_seat_id = None
+for row in range(1, 127):
+    for col in range(0, 8):
+        seat_id = row * 8 + col
+        if (row, col) not in seats:
+            if seat_id + 1 in seat_ids and seat_id - 1 in seat_ids:
+                my_seat_id = seat_id
+                break
+
+    if my_seat_id:
         break
 
 # Answer Two
