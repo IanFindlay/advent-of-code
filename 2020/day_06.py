6f707dee24ef9d86cc6a61eb06f98307b264e821
Ian Findlay <code@findlayian.com>
2020-12-06 10:58:43 +0000

    Add day 6 part two solution

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -4,7 +4,7 @@
 
 
 with open ('input.txt', 'r') as forms:
-    groups = [group_answers for group_answers in forms.read().split('\n\n')]
+    groups = [group_answers.strip() for group_answers in forms.read().split('\n\n')]
 
 overall_yes = 0
 for group in groups:
@@ -17,3 +17,21 @@ for group in groups:
 
 # Answer One
 print("Sum of all groups yes counts:", overall_yes)
+
+overall_group_yes = 0
+for group in groups:
+    group_yes = set()
+    set_initialised = False
+    for member_yes in group.split('\n'):
+        member_yes_set = set([char for char in member_yes])
+
+        if not set_initialised:
+            group_yes = member_yes_set
+            set_initialised = True
+        else:
+            group_yes = member_yes_set.intersection(group_yes)
+
+    overall_group_yes += len(group_yes)
+
+# Answer Two
+print("Sum of all group overall yes counts:", overall_group_yes)
