0d77757e18f1c05b841673becf63c9441d772c99
Ian Findlay <code@findlayian.com>
2020-12-11 18:54:59 +0000

    Add day 11 part two solution

--- 2020/day_11.py
+++ 2020/day_11.py
@@ -45,3 +45,54 @@ for state in new_states.values():
 
 # Answer One
 print("Number of seats that end up occupied:", occupied)
+
+floorplan_dict = {}
+for row in range(len(floorplan)):
+    for col in range(len(floorplan[0])):
+        floorplan_dict[(row, col)] = floorplan[row][col]
+
+while True:
+    new_states = {}
+    for coords, state in floorplan_dict.items():
+        adjacent = [(0, -1), (0, 1), (-1, 0), (1, 0),
+                    (-1, -1), (-1, 1), (1, 1), (1, -1)]
+
+        occupied = 0
+        for seat in adjacent:
+            new_coords = coords
+            while True:
+                new_coords = (new_coords[0] + seat[0], new_coords[1] + seat[1])
+                adj_state = floorplan_dict.get(new_coords, '.')
+                if new_coords[0] < 0 or new_coords[1] < 0:
+                    break
+                if (new_coords[0] == len(floorplan)
+                    or new_coords[1] == len(floorplan[0])):
+                    break
+                if adj_state == 'L':
+                    break
+
+                if adj_state == '#':
+                    occupied += 1
+                    break
+
+                continue
+
+        if state == 'L' and occupied == 0:
+            new_states[coords] = '#'
+        elif state == '#' and occupied >= 5:
+            new_states[coords] = 'L'
+        else:
+            new_states[coords] = state
+
+    if floorplan_dict == new_states:
+        break
+
+    floorplan_dict = new_states
+
+occupied = 0
+for state in new_states.values():
+    if state == '#':
+        occupied += 1
+
+# Answer Two
+print("Number of seats that end up occupied:", occupied)
