5c92eb80b78908e7f9e8fc0ecb1e230a9fe10441
Ian Findlay <code@findlayian.com>
2020-12-12 12:00:16 +0000

    Add day 12 part two solution

--- 2020/day_12.py
+++ 2020/day_12.py
@@ -39,3 +39,37 @@ for instruction in instructions:
 # Answer One
 print("Manhattan distance from starting point:",
       sum([abs(coord) for coord in coords]))
+
+coords = [0, 0]
+waypoint = [1, 10]
+for instruction in instructions:
+    action = instruction[0]
+    value = int(instruction[1:])
+
+    if action == 'N':
+        waypoint[0] += value
+    elif action == 'S':
+        waypoint[0] -= value
+    elif action == 'E':
+        waypoint[1] += value
+    elif action == 'W':
+        waypoint[1] -= value
+    elif action == 'L':
+        for n in range(value // 90):
+            new_waypoint = [None, None]
+            new_waypoint[1] = -waypoint[0]
+            new_waypoint[0] = waypoint[1]
+            waypoint = new_waypoint
+    elif action == 'R':
+        for n in range(value // 90):
+            new_waypoint = [None, None]
+            new_waypoint[1] = waypoint[0]
+            new_waypoint[0] = -waypoint[1]
+            waypoint = new_waypoint
+    elif action == 'F':
+        coords = [coords[0] + waypoint[0] * value,
+                  coords[1] + waypoint[1] * value]
+
+# Answer Two
+print("Actual Manhattan distance from starting point:",
+      sum([abs(coord) for coord in coords]))
