b3d6cf11b64fe13c18337fae7133ea12c13069e4
Ian Findlay <code@findlayian.com>
2020-12-14 14:08:32 +0000

    Add day 14 part two solution

--- 2020/day_14.py
+++ 2020/day_14.py
@@ -2,6 +2,7 @@
 
 """Advent of Code 2020 Day 14 - Docking Data."""
 
+from itertools import product
 import re
 
 with open ('input.txt', 'r') as f:
@@ -26,4 +27,35 @@ for row in rows:
 
 # Answer One
 print("Sum of all values left in memory after initialisation:",
+      sum(memory.values()))
+
+memory = {}
+instruction_regex = re.compile('mem\[(\d*)\]\s=\s(\d*)')
+for row in rows:
+    if row.startswith('mask'):
+        mask = row.split()[2]
+    else:
+        mem, value = instruction_regex.findall(row)[0]
+        mem = format(int(mem), '036b')
+        masked_mem = ''
+        for index, bit in enumerate(mask):
+            if bit == '0':
+                masked_mem += mem[index]
+            else:
+                masked_mem += bit
+
+        x_count = masked_mem.count('X')
+        for floats in product(('0', '1'), repeat=x_count):
+            floats = list(floats)
+            float_mem = ''
+            for bit in masked_mem:
+                if bit == 'X':
+                    float_mem += floats.pop()
+                else:
+                    float_mem += bit
+
+            memory[int(float_mem, 2)] = int(value)
+
+# Answer Two
+print("Sum of all values left in memory after Version 2 initialisation:",
       sum(memory.values()))
