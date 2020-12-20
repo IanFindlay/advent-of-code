68185f097d482683903c4d6754704f3173c99c6b
Ian Findlay <code@findlayian.com>
2020-12-20 21:01:43 +0000

    Add day 20 part one solution

--- 2020/day_20.py
+++ 2020/day_20.py
@@ -15,8 +15,7 @@ class image_tile:
         self.tile = tile
         self.patterns = self.generate_patterns()
 
-        # Make dict of pattern: (top, right, bottom, left) edges
-        pattern_edges = self.map_pattern_to_edges()
+        self.pattern_to_edges = self.map_pattern_to_edges()
 
     def generate_patterns(self):
         """."""
@@ -62,19 +61,74 @@ class image_tile:
             right = [row[0] for row in pattern]
             bottom = pattern[-1]
             left = [row[-1] for row in pattern]
-            pattern_to_edges[index] = (top, right, bottom, left)
+            pattern_to_edges[index] = {'top': top, 'right': right,
+                                       'bot': bottom, 'left': left
+            }
 
         return pattern_to_edges
 
 
+def generate_image_dfs(tiles):
+    """."""
+    size = isqrt(len(tiles))
+    image = []
+    for _ in range(size**2):
+        image.append([])
+
+    stack = []
+    for tile in tiles:
+        for index, _ in enumerate(tile.patterns):
+            image_copy = image.copy()
+            image_copy[0] = (tile, index)
+            stack.append((image_copy, set([tile])))
+
+    while stack:
+        image, used_tiles = stack.pop()
+
+        current_index = len(used_tiles)
+        x, y = divmod(current_index, size)
+
+        above_pattern = None
+        left_pattern = None
+
+        if x != 0:
+            above_tile, index = image[current_index - size]
+            if above_tile:
+                above_pattern = above_tile.pattern_to_edges[index]['bot']
+
+        if y != 0:
+            left_tile, index = image[current_index - 1]
+            if left_tile:
+                left_pattern = left_tile.pattern_to_edges[index]['right']
+
+        for tile in tiles:
+            if tile in used_tiles:
+                continue
+
+            for index, _ in enumerate(tile.patterns):
+
+                if above_pattern:
+                    if tile.pattern_to_edges[index]['top'] != above_pattern:
+                        continue
+
+                if left_pattern:
+                    if tile.pattern_to_edges[index]['left'] != left_pattern:
+                        continue
+
+                image[current_index] = (tile, index)
+                used_tiles.add(tile)
+                stack.append((image.copy(), used_tiles))
+
+                if x == size - 1 and y == size - 1:
+                    return image
 
 
 
-with open('input.txt') as f:
-#with open('inputs/2020_20.txt') as f:
+
+#with open('input.txt') as f:
+with open('inputs/2020_20.txt') as f:
     raw_tiles = f.read().split('\n\n')
 
-size = isqrt(len(raw_tiles))
 tiles = []
 for tile in raw_tiles:
     tile = tile.split('\n')
@@ -88,5 +142,12 @@ for tile in raw_tiles:
         split_tile.append(split_row)
     tiles.append(image_tile(title, split_tile))
 
-for tile in tiles:
-    print(len(tile.patterns))
+size = isqrt(len(tiles))
+reassembled_image = generate_image_dfs(tiles)
+corners = [0, -1, 0 + size - 1, (len(tiles) -1) - (size - 1)]
+corners_product = 1
+for corner in corners:
+    corners_product *= reassembled_image[corner][0].id_num
+
+# Answer One
+print("Product of corner ids of reassembled_image", corners_product)
