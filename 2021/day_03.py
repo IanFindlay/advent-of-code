4bf1d5743b198da7ed0d63f87f610a8217954114
Ian Findlay <code@findlayian.com>
2021-12-03 15:37:19 +0000

    Write docstring and make minor changes to code

--- 2021/day_03.py
+++ 2021/day_03.py
@@ -4,12 +4,12 @@
 
 
 def convert_to_columns(binary_values):
-    """."""
+    """Takes list of strings and returns one with rows and columns swapped."""
     column_conversion = []
-    for row in range(len(binary_values[0])):
+    for row_num in range(len(binary_values[0])):
         column_conversion.append('')
-        for column in range(len(binary_values)):
-            column_conversion[-1] += binary_values[column][row]
+        for col_num in range(len(binary_values)):
+            column_conversion[-1] += binary_values[col_num][row_num]
 
     return column_conversion
 
@@ -19,8 +19,8 @@ with open('inputs/day_03.txt', 'r') as aoc_input:
 
 binary_columns = convert_to_columns(input_lines)
 gamma = ''
-for i in range(len(binary_columns)):
-    if binary_columns[i].count('0') > len(binary_columns[i]) / 2:
+for column in binary_columns:
+    if column.count('0') > len(column) / 2:
         gamma += '0'
     else:
         gamma += '1'
@@ -30,37 +30,35 @@ epsilon = ''.join(['1' if bit == '0' else '0' for bit in gamma])
 # Answer One
 print("Submarine power consumption:", int(gamma, 2) * int(epsilon, 2))
 
-oxygen_binary = input_lines.copy()
-oxygen_columns = binary_columns.copy()
-oxygen = ''
+oxy_binaries = input_lines.copy()
+oxy_columns = binary_columns.copy()
+oxy_criteria = ''
 i = 0
-while len(oxygen_binary) > 1:
-
-    if oxygen_columns[i].count('1') >= len(oxygen_columns[i]) / 2:
-        oxygen += '1'
+while len(oxy_binaries) > 1:
+    if oxy_columns[i].count('1') >= len(oxy_columns[i]) / 2:
+        oxy_criteria += '1'
     else:
-        oxygen += '0'
+        oxy_criteria += '0'
 
-    oxygen_binary = list(filter(
-            lambda binary: binary.startswith(oxygen), oxygen_binary))
+    oxy_binaries = list(filter(
+            lambda binary: binary.startswith(oxy_criteria), oxy_binaries))
 
-    oxygen_columns = convert_to_columns(oxygen_binary)
+    oxy_columns = convert_to_columns(oxy_binaries)
 
     i += 1
 
 co2_binary = input_lines.copy()
 co2_columns = binary_columns.copy()
-co2 = ''
+co2_criteria = ''
 i = 0
 while len(co2_binary) > 1:
-
     if co2_columns[i].count('1') >= len(co2_columns[i]) / 2:
-        co2 += '0'
+        co2_criteria += '0'
     else:
-        co2 += '1'
+        co2_criteria += '1'
 
     co2_binary = list(filter(
-            lambda binary: binary.startswith(co2), co2_binary))
+            lambda binary: binary.startswith(co2_criteria), co2_binary))
 
     co2_columns = convert_to_columns(co2_binary)
 
@@ -68,4 +66,4 @@ while len(co2_binary) > 1:
 
 # Answer Two
 print("Submarine life support rating:",
-      int(oxygen_binary[0], 2) * int(co2_binary[0], 2))
+      int(oxy_binaries[0], 2) * int(co2_binary[0], 2))
