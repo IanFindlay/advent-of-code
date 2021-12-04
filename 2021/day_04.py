161014087343a455a92fa7967658e0916223b252
Ian Findlay <code@findlayian.com>
2021-12-04 20:50:07 +0000

    Refactor and clarify solutions

--- 2021/day_04.py
+++ 2021/day_04.py
@@ -7,86 +7,72 @@ import re
 
 
 def mark_cards(cards, number):
-    cards_copy = cards.copy()
-    for card_index, card in enumerate(cards):
+    """Go through cards list marking off number whenever it appears."""
+    for card in cards:
         for row_index, row in enumerate(card):
-            for col_index, card_num in enumerate(row):
-                if number == card_num:
-                    cards_copy[card_index][row_index][col_index] = None
+            card[row_index] = [x if x != number else None for x in row]
 
-    return cards_copy
+    return cards
 
 
 def check_for_winner(cards):
+    """Return indicies of winning cards i.e. has a completed row or column."""
     winners = set()
     for card_num, card in enumerate(cards):
+        row_winner = False
         for row in card:
             if len(set(row)) == 1:
                 winners.add(card_num)
+                row_winner = True
+                break
+
+        if row_winner:
+            continue
 
         for col_index in range(len(card[0])):
             column = []
             for row in card:
                 column.append(row[col_index])
+
             if len(set(column)) == 1:
                 winners.add(card_num)
+                break
 
-    if winners:
-        return winners
-
-    return False
+    return winners
 
 
 with open('inputs/day_04.txt', 'r') as aoc_input:
     sections = aoc_input.read().strip().split('\n\n')
 
-balls = []
-cards = []
-for part in sections:
-    if not balls:
-        balls = [int(x) for x in part.split(',')]
-        continue
+balls = [int(x) for x in sections[0].split(',')]
 
+cards = []
+for part in sections[1:]:
     cards.append([])
-    rows = part.split('\n')
-    for row in rows:
+    for row in part.split('\n'):
         cards[-1].append([int(x) for x in re.findall(r'\d\d?', row)])
 
-num_cards = len(cards)
-
-ball = 0
-winning_cards = []
+completed_cards = []
 for ball in balls:
     cards = mark_cards(cards, ball)
 
     winning_indicies = check_for_winner(cards)
-    if winning_indicies is not False:
-        for index in winning_indicies:
-            winning_cards.append((cards[index].copy(), ball))
-
-        cards_copy = []
-        for card_index, card in enumerate(cards):
-            if card_index not in winning_indicies:
-                cards_copy.append(card)
-
-
-        cards = cards_copy
+    if winning_indicies:
+        for index in sorted(winning_indicies, reverse=True):
+            completed_cards.append((cards[index], ball))
+            del cards[index]
 
         if len(cards) == 0:
             break
 
-winning_card, winning_ball = winning_cards[0]
-winner_sum = 0
-for row in winning_card:
-    winner_sum += sum([x for x in row if x != None])
+winning_card, winning_ball = completed_cards[0]
+winning_sum = sum([sum([x for x in row if x != None]) for row in winning_card])
 
 # Answer One
-print("Final score of winning card:", winner_sum * winning_ball)
+print("Final score of winning card:", winning_sum * winning_ball)
 
-losing_card, losing_ball = winning_cards[-1]
-losing_sum = 0
-for row in losing_card:
-    losing_sum += sum([x for x in row if x != None])
+losing_card, losing_ball = completed_cards[-1]
+losing_sum = sum([sum([x for x in row if x != None]) for row in losing_card])
 
 # Answer Two
 print("Final score of losing card:", losing_sum * losing_ball)
