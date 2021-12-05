9b5f5d9eb0837747ceca412d84e63162f0908de9
Ian Findlay <code@findlayian.com>
2021-12-05 10:04:54 +0000

    Refactor 2021 day 5

--- 2021/day_05.py
+++ 2021/day_05.py
@@ -5,71 +5,59 @@
 
 import re
 
+
 with open('inputs/day_05.txt', 'r') as aoc_input:
     lines = [re.findall(r'(\d*),(\d*)', x) for x in aoc_input.readlines()]
     lines = [[(int(x), int(y)) for x, y in tup] for tup in lines]
 
 coords_dict = {}
 for line in lines:
-    current_x = line[0][0]
-    current_y = line[0][1]
-    end_x = line[1][0]
-    end_y = line[1][1]
-    initial_x = current_x
+    current_x, current_y = line[0]
+    end_x, end_y = line[1]
+    initial_x = current_x  # Needed for column loop entering logic
 
-    while True:
-        if current_y != end_y:
-            break
+    if current_y == end_y:
 
-        coords = (current_x, current_y)
-        coord_value = coords_dict.get(coords, '.')
+        while True:
+            coords = (current_x, current_y)
+            if coords_dict.get(coords, '.') == '.':
+                coords_dict[coords] = 1
+            else:
+                coords_dict[coords] += 1
 
-        if coord_value == '.':
-            coords_dict[coords] = 1
-        else:
-            coords_dict[coords] += 1
+            if current_x == end_x:
+                break
 
-        if current_x == end_x:
-            break
+            if current_x < end_x:
+                current_x += 1
+            else:
+                current_x -= 1
 
-        if current_x < end_x:
-            current_x += 1
-        else:
-            current_x -= 1
+    if initial_x == end_x:
 
-    while True:
-        if initial_x != end_x:
-            break
+        while True:
+            coords = (current_x, current_y)
+            if coords_dict.get(coords, '.') == '.':
+                coords_dict[coords] = 1
+            else:
+                coords_dict[coords] += 1
 
-        coords = (current_x, current_y)
-        coord_value = coords_dict.get(coords, '.')
+            if current_y == end_y:
+                break
 
-        if coord_value == '.':
-            coords_dict[coords] = 1
-        else:
-            coords_dict[coords] += 1
+            if current_y < end_y:
+                current_y += 1
+            else:
+                current_y -= 1
 
-        if current_y == end_y:
-            break
-
-        if current_y < end_y:
-            current_y += 1
-        else:
-            current_y -= 1
-
-dangerous_zones = 0
-for coords, value in coords_dict.items():
-    if value > 1:
-        dangerous_zones += 1
+dangerous_zones = sum([1 if x > 1 else 0 for x in coords_dict.values()])
 
 # Answer One
 print("Number of dangerous zones:", dangerous_zones)
 
 for line in lines:
-    current_x = line[0][0]
-    current_y = line[0][1]
-    end_x = line[1][0]
-    end_y = line[1][1]
+    current_x, current_y = line[0]
+    end_x, end_y = line[1]
 
     # Already been processed in part one
     if current_x == end_x or current_y == end_y:
@@ -77,9 +65,7 @@ for line in lines:
 
     while True:
         coords = (current_x, current_y)
-        coord_value = coords_dict.get(coords, '.')
-
-        if coord_value == '.':
+        if coords_dict.get(coords, '.') == '.':
             coords_dict[coords] = 1
         else:
             coords_dict[coords] += 1
@@ -98,10 +84,7 @@ for line in lines:
             current_y -= 1
 
 
-dangerous_zones = 0
-for coords, value in coords_dict.items():
-    if value > 1:
-        dangerous_zones += 1
+dangerous_zones = sum([1 if x > 1 else 0 for x in coords_dict.values()])
 
 # Answer Two
 print("Number of dangerous zones including diagonals:", dangerous_zones)
