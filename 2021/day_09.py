38c0637491a97ac58e206e152041bfb5a07d3e85
Ian Findlay <code@findlayian.com>
2021-12-09 12:06:46 +0000

    Add AoC 2021 day 9 part two solution

--- 2021/day_09.py
+++ 2021/day_09.py
@@ -11,7 +11,8 @@ for y, column in enumerate(lines):
     for x, row in enumerate(column):
         height_map[(x, y)] = int(row)
 
-low_points = 0
+low_points = []
+low_points_sum = 0
 for coords, height in height_map.items():
     x, y = coords
     adjacent = ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
@@ -22,7 +23,34 @@ for coords, height in height_map.items():
             break
 
     if lowest:
-        low_points += height + 1
+        low_points.append(coords)
+        low_points_sum += height + 1
 
 # Answer One
-print("Sum of risk levels of low points", low_points)
+print("Sum of risk levels of low points", low_points_sum)
+
+basin_sizes = []
+for low_point in low_points:
+    part_of_basin = set([low_point])
+    coords_to_check_stack = [low_point]
+
+    while coords_to_check_stack:
+
+        x, y = coords_to_check_stack.pop()
+        adjacent = ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
+        for neighbour in adjacent:
+            neighbour_height = height_map.get(neighbour, 0)
+            if neighbour_height > height_map[(x, y)] and neighbour_height != 9:
+                part_of_basin.add(neighbour)
+                coords_to_check_stack.append(neighbour)
+
+    basin_sizes.append(len(part_of_basin))
+
+basin_sizes.sort(reverse=True)
+
+largest_three_product = 1
+for size in basin_sizes[:3]:
+    largest_three_product *= size
+
+# Answer Two
+print("Product of three largest basin sizes:", largest_three_product)
