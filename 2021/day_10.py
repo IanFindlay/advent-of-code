9455dd895b4239bdeed90491f0b821f2621e5e01
Ian Findlay <code@findlayian.com>
2021-12-10 13:59:53 +0000

    Add AoC 2021 day 10 part two and update README

--- 2021/day_10.py
+++ 2021/day_10.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Advent of Code 2021 Day 10 - """
+"""Advent of Code 2021 Day 10 - Syntax Scoring"""
 
 
 with open('inputs/day_10.txt', 'r') as aoc_input:
@@ -11,24 +11,57 @@ match_dict = {'(': ')', '{': '}', '<': '>', '[': ']'}
 illegal_char_points = {')': 3, ']': 57, '}': 1197, '>': 25137}
 
 corrupted = []
-for line in lines:
+i = len(lines)
+while i >= 0:
+    i -= 1
     open_chunk_symbols = []
     line_corrupted = False
-    for char in line:
+    for char in lines[i]:
         if char in opening_chars:
             open_chunk_symbols.append(char)
         else:
             if char != match_dict[open_chunk_symbols.pop()]:
                 line_corrupted = True
+                del lines[i]
 
 
         if line_corrupted:
-            corrupted.append((line, char))
+            corrupted.append(char)
             break
 
 syntax_error_score = 0
-for line in corrupted:
-    syntax_error_score += illegal_char_points[line[1]]
+for char in corrupted:
+    syntax_error_score += illegal_char_points[char]
 
 # Answer One
 print("Total syntax error score:", syntax_error_score)
+
+completion_strings = []
+for line in lines:
+    open_chunk_symbols = []
+    for char in line:
+        if char in opening_chars:
+            open_chunk_symbols.append(char)
+        else:
+            open_chunk_symbols.pop()
+
+    finishing_chars = ''
+    for remaining in open_chunk_symbols:
+        finishing_chars = match_dict[remaining] + finishing_chars
+
+    completion_strings.append(finishing_chars)
+
+
+completion_char_points = {')': 1, ']': 2, '}': 3, '>': 4}
+completion_scores = []
+for completion_string in completion_strings:
+    completion_score = 0
+    for char in completion_string:
+        completion_score *= 5
+        completion_score += completion_char_points[char]
+
+    completion_scores.append(completion_score)
+
+# Answer Two
+print("Middle completion score:",
+        sorted(completion_scores)[len(completion_scores) // 2])
