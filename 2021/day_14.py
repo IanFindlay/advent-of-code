96bea97b9330451bd1e54bf0f6a98a438695df2e
Ian Findlay <code@findlayian.com>
2021-12-14 14:29:13 +0000

    Add AoC day 14 part two and update README stars

--- 2021/day_14.py
+++ 2021/day_14.py
@@ -27,5 +27,41 @@ for element in polymer:
 
 
 most_minus_least = max(element_count.values()) - min(element_count.values())
+
 # Answer One
-print("Quantity of most common elemnet minus least common:", most_minus_least)
+print("Quantity of most common element minus least common:", most_minus_least)
+
+pairs = {}
+polymer = template.strip()
+for index, element in enumerate(polymer[:-1]):
+    pair = f'{element}{polymer[index + 1]}'
+    pairs[pair] = pairs.get(pair, 0) + 1
+
+for _ in range(40):
+    new_pairs = {}
+    for pair in pairs:
+        if pairs[pair] == 0:
+            continue
+
+        element_one, element_two = pair
+        to_insert = reactions[pair]
+        first_new = f'{element_one}{to_insert}'
+        second_new = f'{to_insert}{element_two}'
+
+        new_pairs[first_new] = pairs[pair] + new_pairs.get(first_new, 0)
+        new_pairs[second_new] = pairs[pair] + new_pairs.get(second_new, 0)
+
+    pairs = new_pairs
+
+element_count = {}
+for pair, count in pairs.items():
+    element_one, element_two = pair
+    element_count[element_one] = element_count.get(element_one, 0) + count
+
+# Last element stays same throughout but isn't counted in above
+element_count[polymer[-1]] += 1
+
+most_minus_least = max(element_count.values()) - min(element_count.values())
+
+# Answer Two
+print("Quantity of most common element minus least common:", most_minus_least)
