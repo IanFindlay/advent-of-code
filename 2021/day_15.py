12b0b7a226b559dd1eee9eb49cedc2df9e85ea4e
Ian Findlay <code@findlayian.com>
2021-12-15 12:42:30 +0000

    Refactor AoC 2021 day 15 - clean up and speed up

--- 2021/day_15.py
+++ 2021/day_15.py
@@ -4,36 +4,36 @@
 
 
 def dijkstra(map_dict, end_coords):
-    """."""
     unvisited = set(map_dict.keys())
-    node_dict = {}
-    node_dict[(0, 0)] = 0
-
+    node_dict = {(0, 0): 0}
     current_node = (0, 0)
-    while True:
+    while node_dict:
         x, y = current_node
-        adjacent = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        risk = node_dict[current_node]
 
+        adjacent = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
         for next_to in adjacent:
             if next_to not in unvisited:
                 continue
 
-            risk = node_dict[current_node] + map_dict[next_to]
-            if not node_dict.get(next_to, None) or risk < node_dict[next_to]:
-                node_dict[next_to] = risk
+            current_risk = node_dict.get(next_to, None)
+            new_risk = risk + map_dict[next_to]
+            if not current_risk or new_risk < current_risk:
+                node_dict[next_to] = new_risk
 
-        unvisited.remove((x, y))
-        del node_dict[(x, y)]
+        unvisited.remove(current_node)
+        del node_dict[current_node]
 
-        next_node = None
+        lowest_risk = min(node_dict.values())
         for coords, risk in node_dict.items():
-            if not next_node or node_dict[next_node] > risk:
-                next_node = coords
+            if risk == lowest_risk:
+                current_node = coords
+                break
 
-        if next_node == end_coords or not next_node:
+        if current_node == end_coords:
             return node_dict[end_coords]
 
-        current_node = next_node
+    return None
 
 
 with open('inputs/day_15.txt', 'r') as aoc_input:
@@ -49,36 +49,28 @@ end_coords = (len(lines) - 1, len(lines[0]) - 1)
 # Answer One
 print("Lowest total risk of any path:", dijkstra(map_dict, end_coords))
 
-# Build extended map
-extended_map = map_dict.copy()
+# Build full map
+full_map = map_dict.copy()
 for coords, risk in map_dict.items():
-
     x, y = coords
-
     for i in range(1, 5):
         new_risk = (risk + i) % 9
-        if new_risk == 0:
-            new_risk = 9
+        new_risk = 9 if new_risk == 0 else new_risk
 
         new_x = x + ((end_coords[0] + 1) * i)
-        extended_map[(new_x, y)] = new_risk
+        full_map[(new_x, y)] = new_risk
 
-map_dict = extended_map.copy()
+map_dict = full_map.copy()
 for coords, risk in map_dict.items():
-
     x, y = coords
-
     for i in range(1, 5):
         new_risk = (risk + i) % 9
-        if new_risk == 0:
-            new_risk = 9
+        new_risk = 9 if new_risk == 0 else new_risk
 
         new_y = y + ((end_coords[1] + 1) * i)
-        extended_map[(x, new_y)] = new_risk
+        full_map[(x, new_y)] = new_risk
 
 new_end_coords = ((end_coords[0] + 1) * 5 - 1, (end_coords[1] + 1) * 5 - 1)
 
 # Answer Two
-print("Lowest total risk of any path on full map:",
-       dijkstra(extended_map, new_end_coords)
-)
+print("Lowest total risk on full map:", dijkstra(full_map, new_end_coords))
