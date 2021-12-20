d69cd956dcc6bbd85cff28d9e48a156295b1f46e
Ian Findlay <code@findlayian.com>
2021-12-20 15:17:53 +0000

    Switch to set as value doesn't need to be stored

--- 2021/day_20.py
+++ 2021/day_20.py
@@ -6,40 +6,40 @@
 with open('../inputs/day_20.txt', 'r') as aoc_input:
     algo, image = [x.strip() for x in aoc_input.read().split('\n\n')]
 
-def print_image(image_dict):
-    min_x, max_x, min_y, max_y = get_image_ranges(image_dict)
+def print_image(image_coords):
+    min_x, max_x, min_y, max_y = get_image_ranges(image_coords)
     for y in range(min_y, max_y + 1):
         row = ''
         for x in range(min_x, max_x + 1):
-            row += image_dict.get((x, y), '.')
+            row += '#' if (x, y) in image_coords else '.'
         print(row)
     print()
 
-    lit = [1 for x in image_dict.values() if x == '#']
-    print("Number of lit:", sum(lit))
+    print("Number of lit:", len(image_coords))
 
 
-def get_image_ranges(image_dict):
-    x_values = sorted([coords[0] for coords in image_dict.keys()])
-    y_values = sorted([coords[1] for coords in image_dict.keys()])
+def get_image_ranges(image_coords):
+    x_values = sorted([coords[0] for coords in image_coords])
+    y_values = sorted([coords[1] for coords in image_coords])
     min_x, max_x = x_values[0] - 1, x_values[-1] + 1
     min_y, max_y = y_values[0] - 1, y_values[-1] + 1
 
     return (min_x, max_x, min_y, max_y)
 
-image_coords = {}
+image_coords = set()
 for row_num, row in enumerate(image.split('\n')):
     for col_num, value in enumerate(row):
         if value == '#':
-            image_coords[(col_num, row_num)] = value
+            image_coords.add((col_num, row_num))
 
 print_image(image_coords)
 input()
 
-for _ in range(2):
+"""Actual input has algo[0] mapped to # so the infinite matters"""
+for step in range(2):
     min_x, max_x, min_y, max_y = get_image_ranges(image_coords)
 
-    new_image = {}
+    new_image = set()
     for y in range(min_y, max_y + 1):
 
         for x in range(min_x, max_x + 1):
@@ -52,18 +52,18 @@ for _ in range(2):
 
             binary = ''
             for pixel in pixels:
-                value = image_coords.get(pixel, '.')
-                binary += '1' if value == '#' else '0'
+                binary += '1' if pixel in image_coords else '0'
 
             new_value = algo[int(binary, 2)]
-            new_image[(x, y)] = new_value
+            if new_value == '#':
+                new_image.add((x, y))
 
     print_image(new_image)
     input()
 
     image_coords = new_image
 
-lit = [1 for x in image_coords.values() if x == '#']
+lit = [1 for x in image_coords if x == '#']
 # Answer One
 
 print("Number of pixels lit in resulting image:", sum(lit))
